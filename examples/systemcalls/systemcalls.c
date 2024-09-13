2d045a6fc924d68ca00ed33f96d158f39f1ed071
Amr-Khaled-BS <amr.khaled@brightskiesinc.com>
2024-09-13 15:18:44 +0300

    basic assignment submission

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,12 @@
 #include "systemcalls.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "errno.h"
+#include "syslog.h"
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,8 +24,7 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    return !system(cmd);
 }
 
 /**
@@ -58,6 +65,17 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int status;
+    pid_t pid = fork ();
+    if (pid<0) {
+        return false;
+    }
+    else if (pid==0) {
+        if( execv(command[0], command) ){
+            return false;
+        }
+        waitpid(pid, &status, WNOHANG);
+    }
 
     va_end(args);
 
@@ -92,6 +110,32 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int kidpid;
+    int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd < 0)
+    {
+        perror("open");
+        return false;
+    }
+    switch (kidpid = fork())
+    {
+    case -1:
+        perror("fork");
+        return false;
+    case 0:
+        if (dup2(fd, 1) < 0)
+        {
+            perror("dup2");
+            return false;
+        }
+        close(fd);
+        execv(command[0], command);
+        // perror("execvp");
+        // return false;
+    default:
+        close(fd);
+        /* do whatever the parent wants to do. */
+    }
 
     va_end(args);
 
