4cba4746be170809d4965fd7048a2d847df9ea79
Amr-Khaled-BS <amr.khaled@brightskiesinc.com>
2024-09-16 15:54:56 +0300

    assignment 3 part 2 commit

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+export PATH=/home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/:$PATH
 
 if [ $# -lt 1 ]
 then
@@ -35,9 +36,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # export PATH=$PATH:/home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    # echo "6"
+    # QEMU_AUDIO_DRV=none qmeu-system-arm -m 265M -nographic -M versatilepb -kernel zImage
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +58,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+if [ ! -d "${OUTDIR}/rootfs" ]
+then
+    mkdir -p rootfs
+    cd rootfs
+    mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+    mkdir -p usr/bin usr/lib usr/sbin
+    mkdir -p var/lg
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +74,51 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    # make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd ../rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+# find /home/amr/installations/ -name "libm.so.6"
+cp /home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64
+cp /home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp /home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp /home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /home/amr/installations/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}gcc
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app/writer ${OUTDIR}/rootfs/home
+cp ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app/finder.sh ${OUTDIR}/rootfs/home
+cp -r ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app/conf/ ${OUTDIR}/rootfs/home/
+cp ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app/finder-test.sh ${OUTDIR}/rootfs/home
+cp ~/projects/coursera_emb_linux/assignments/assignment-1-Amr-Khaled-BS/finder-app/autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ../initramfs.cpio
