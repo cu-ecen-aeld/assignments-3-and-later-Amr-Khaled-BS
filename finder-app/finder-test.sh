5cbeab2c0b03dc3695ed3502b9d28ae3869c8c18
Amr-Khaled-BS <amr.khaled@brightskiesinc.com>
2024-09-20 16:46:34 +0300

    prepare finder-test.sh for assignment 4-2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -7,8 +7,11 @@ set -u
 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# WRITEDIR=/tmp/aeld-data
+WRITEDIR=/tmp/assignment4-result.txt
+username=$(cat /etc/finder-app/conf/username.txt)
+writerapp=/usr/bin/writer
+finderapp=/usr/bin/finder.sh
 
 if [ $# -lt 3 ]
 then
@@ -56,10 +59,10 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	# ./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${writerapp} "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${finderapp} "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
